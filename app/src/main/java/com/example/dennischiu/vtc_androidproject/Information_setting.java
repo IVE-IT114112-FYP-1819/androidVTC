bfa91ac446b32968167c44f6d093603cb7f5a5c0
dennischiuu <dennis12345671@outlook.com>
2018-11-02 13:09:44 +0800

    intent and checking

--- app/src/main/java/com/example/dennischiu/vtc_androidproject/Information_setting.java
+++ app/src/main/java/com/example/dennischiu/vtc_androidproject/Information_setting.java
@@ -11,12 +11,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Information_setting extends AppCompatActivity {
 
     EditText mEditText_Firstname;
     EditText mEditText_Lastname;
     EditText mEditText_Phone;
+    TextView mErrorTest;
     ImageButton mButton_Save;
     ImageButton mSetAlert;
 
@@ -30,6 +32,7 @@ public class Information_setting extends AppCompatActivity {
         mEditText_Phone = findViewById(R.id.et_phone);
         mButton_Save = findViewById(R.id.btn_save);
         mSetAlert = findViewById(R.id.ib_setAlert);
+        mErrorTest = findViewById(R.id.error_message);
 
         String firstname = getSharedPreferences("Information", MODE_PRIVATE)
                 .getString("firstname", "");
@@ -47,23 +50,40 @@ public class Information_setting extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String firstname = mEditText_Firstname.getText().toString().trim();
-                String lastname = mEditText_Lastname.getText().toString().trim();
-                String phone = mEditText_Phone.getText().toString().trim();
-                SharedPreferences pref = getSharedPreferences("Information", MODE_PRIVATE);
-                pref.edit()
-                    .putString("firstname", firstname)
-                    .putString("lastname", lastname)
-                    .putString("phone", phone)
-                    .commit();
+                if (mEditText_Firstname.getText().length() == 0) {
+                    mErrorTest.setVisibility(View.VISIBLE);
+                    mErrorTest.setText("Please Enter Your FirstName");
+                } else if (mEditText_Lastname.getText().length() == 0) {
+                    mErrorTest.setVisibility(View.VISIBLE);
+                    mErrorTest.setText("Please Enter Your LastName");
+                } else if (mEditText_Phone.getText().length() == 0) {
+                    mErrorTest.setVisibility(View.VISIBLE);
+                    mErrorTest.setText("Please Enter Your PhoneNumber");
+                } else {
+
+                    String firstname = mEditText_Firstname.getText().toString().trim();
+                    String lastname = mEditText_Lastname.getText().toString().trim();
+                    String phone = mEditText_Phone.getText().toString().trim();
+                    SharedPreferences pref = getSharedPreferences("Information", MODE_PRIVATE);
+                    pref.edit()
+                        .putString("firstname", firstname)
+                        .putString("lastname", lastname)
+                        .putString("phone", phone)
+                        .commit();
+
+                    Toast.makeText(Information_setting.this, R.string.login_error_message, Toast.LENGTH_SHORT)
+                         .show();
+                }
 
             }
         });
 
-        mSetAlert.setOnClickListener(v -> {
-            Intent intent = new Intent(this, SetAlarm.class);
-            startActivity(intent);
-        });
+        mSetAlert.setOnClickListener(v ->
+
+                                     {
+                                         Intent intent = new Intent(this, SetAlarm.class);
+                                         startActivity(intent);
+                                     });
 
 
     }
